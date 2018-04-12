e6f3719ab66bfda016b5a8a57ffdb5a6e97f86dc
luisricardo <luisricardo@Jessica77>
2018-04-12 10:30:09 -0600

    Agregar Documentacion

--- src/ejemplopractica/EjemploPractica.java
+++ src/ejemplopractica/EjemploPractica.java
@@ -1,19 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplopractica;
 
 /**
- *
+ * 
+ *Nombre de la clase: ejemplopractico
+ * Fecha: 12-04-2018
+ * Version: 1.0
+ * CopyRight: Jessica
  * @author jessica Linares
  */
 public class EjemploPractica {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
